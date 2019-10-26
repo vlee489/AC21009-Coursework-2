2c626d65b57dcf19cc5f17a30911c3f6227f1ea3
Ramsay Sewell <sewellrf@gmail.com>
2019-10-26 16:08:30 +0100

    decimal to binary converter

--- rule.cpp
+++ rule.cpp
@@ -1,3 +1,41 @@
+#include <cmath>
+#include <iostream>
 using namespace std;
 
-// This is a test commit
+void toBinary(int decimal)
+{
+    bool binary[8];
+    int binaryComp[8];
+    binaryComp[0] = 1;
+
+    for (int i = 1; i < 8; i++)
+    {
+        binary[i] = 0;
+        binaryComp[i] = pow(2, i);
+    }
+
+    for (int i = 7; i >= 0; i--)
+    {
+        if (decimal >= binaryComp[i])
+        {
+            decimal = decimal - binaryComp[i];
+            binary[i] = 1;
+        }
+        else
+        {
+            binary[i] = 0;
+        }
+    }
+
+    for (int i = 7; i >= 0; i--)
+    {
+        cout << binary[i];
+    }
+    cout << "" << endl;
+}
+
+int main()
+{
+    toBinary(157);
+    return 0;
+}
