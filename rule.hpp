aeda2785ac791ee6df9f604188c9bf0a90a67de3
Vincent Lee <vlee1@live.co.uk>
2019-10-31 13:49:43 +0000

    comment file

--- rule.hpp
+++ rule.hpp
@@ -5,6 +5,8 @@ class Rule {
   public:
         void toBinary(int binary[], int decimal);
         int toDecimal(bool binary[]);
+        // Ruleset = Binary version of the rules desired
+        // ruleNum = rule but in int form
         void setRule(int ruleset[], int ruleNum);
         bool generateCell(int ruleset[], bool neighbourhood[]);
 };
