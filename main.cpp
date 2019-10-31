c16ebef94f7f0cd95e6b4664b61bb1835dadb9c6
Vincent Lee <vlee1@live.co.uk>
2019-10-31 13:44:42 +0000

    starts of menu

--- main.cpp
+++ main.cpp
@@ -8,17 +8,24 @@ int main() { return 0; }
 int userNumberInput(int min, int max, string message){
     int UserInput;
     bool valid = false;
-    while (true){
+    while (valid == false){
         cout << message + ":";
         cin >> UserInput;
         if (UserInput < max && UserInput > min){
             cout << "/n"
+            valid = true;
             return UserInput;
         } else {
             cout << "\nInvalid input\n"
             cout << "Please enter a whole number between" + min + " and " + max + "\n"
         }
     }
+}
+
+int runProgram(){
+    int rule = userNumberInput(0, 256, "Please enter the desired rule you want to use");
+    int generations = userNumberInput(0, 101, "Please enter the desired generations you want");
+
 }
 
 
