db34aa292d5ab2432ca4c50786baffd9825b241f
Max Kelly <maxwill.kelly@gmail.com>
2019-11-01 00:50:39 +0000

    Error code system

--- error.hpp
+++ error.hpp
@@ -1,6 +1,20 @@
 // Thought it might be a good idea to have an error code system
 // Sounds good, we can add to it as the project goes on
 
+#define SUCCESS 0
+
+// Pointer is unexpectedly Null
+#define UNEXPECTED_NULL_POINTER -1
+
+// 
+#define TABLE_NOT_INITIALISED -101
+#define INVALID_TABLE_PARAMETER -2
+#define Y_INDEX_OUT_OF_BOUNDS -300
+
 // Saving and Loading tables
-#define FILE_NOT_FOUND 100
-#define FILE_NOT_SAVED 101
+#define FILE_NOT_FOUND -102
+#define FILE_NOT_SAVED -103
+
+// Rule File
+#define RULE_NUM_OUT_RANGE -501
+#define DECIMAL_VAL_OUT_OF_RANGE -502
--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <sstream>
 #include <string>
 
+#include "error.hpp"
 #include "rule.hpp"
 #include "storage.hpp"
+
 using namespace std;
 
-int runProgram();
+void runProgram();
 
 int main() {
   runProgram();
@@ -33,25 +35,54 @@ void printArray(T array, int length) {
   cout << endl;
 }
 
-int runProgram() {
+void checkValidity(int valid) {
+  switch(valid) {
+    case SUCCESS:
+      break;
+    case INVALID_TABLE_PARAMETER:
+      cout << "Error: " << endl;
+      break;
+    case RULE_NUM_OUT_RANGE:
+      cout << "Error: " << endl;
+      break;
+    case DECIMAL_VAL_OUT_OF_RANGE:
+      cout << "Error: " << endl;
+      break;
+    case TABLE_NOT_INITIALISED:
+      cout << "Error: Function has been called using an uninitialised table" << endl;
+      break;
+    case Y_INDEX_OUT_OF_BOUNDS:
+      cout << "Error: A value is attempting to be set using a Y co-ordinate that is out of the bounds of the table" << endl;
+      break;
+    case UNEXPECTED_NULL_POINTER:
+      cout << "Error: Function has been called using a null pointer" << endl;
+      break;
+    default:
+      cout << "Error: Invalid Error Code" << endl;
+      break;
+  }
+
+}
+
+void runProgram() {
   int rule = userNumberInput(0, 256, "Please enter the desired rule you want to use");
   int generations = userNumberInput(0, 101, "Please enter the desired generations you want");
   Table fullTable;
-  fullTable.initTable(generations);
+  checkValidity(fullTable.initTable(generations));
   Rule ruleObj;
-  ruleObj.setRule(rule);
+  checkValidity(ruleObj.setRule(rule));
+
   int arrayWidth = fullTable.getArrayWidth();
   for (int row = 1; row < generations; row++) {
     for (int col = 0; col < arrayWidth; col++) {
       bool* neighbourhood = fullTable.getNeighbourhood(col, row);
       bool cell = ruleObj.generateCell(neighbourhood);
-      fullTable.setVal(col, row, cell);
+      checkValidity(fullTable.setVal(col, row, cell));
       delete neighbourhood;
     }
   }
   cout << endl;
-  cout << "Debug Table Check: ";
-  printArray(fullTable.getPTable(), fullTable.getArraySize());
+  // cout << "Debug Table Check: ";
+  // printArray(fullTable.getPTable(), fullTable.getArraySize());
   fullTable.printTable();
-  return 0;
 }
