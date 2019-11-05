e9cdaf9fd8d64df157eaaba600790cd3ae5508ee
Max Kelly <maxwill.kelly@gmail.com>
2019-11-05 16:25:55 +0000

    Partly redesigned main menu

--- main.cpp
+++ main.cpp
@@ -3,27 +3,80 @@
 #include <string>
 #include <vector>
 
+// Imports error codes
 #include "error.hpp"
+// Imports methods which allows the user to set their own generation
 #include "generation.hpp"
+// Imports methods to handle user input and output
 #include "inputOutput.hpp"
+// Imports methods to generate rules and convert numbers to binary and decimal
 #include "rule.hpp"
+// Imports merthods for storing a table of values
 #include "table.hpp"
 
 using namespace std;
 
-void runProgram();
+// Stores pointers for objects of classes
+Table* fullTable;
+Rule* ruleObj;
+Generation* generationObj;
 
+// Headers for the methods in this file
+void initObjects();
+void displayMenu();
+bool processMenu(int choice);
+void createAutomaton();
+void loadAutomaton();
+
+// Main Function
 int main() {
-  runProgram();
+  initObjects();
+  menuInt(displayMenu, processMenu);
   return 0;
 }
 
-void runProgram() {
+// Initialises objects 
+void initObjects() {
+  fullTable = new Table();
+  ruleObj = new Rule();
+  generationObj = new Generation();
+}
+
+void displayMenu() {
+  cout << "Cellular Automaton Program" << endl;
+  cout << "Module Code: AC210009" << endl;
+  cout << "Assignment 2";
+  cout << endl;
+  cout << "" << endl;
+  cout << "1. Create your own cellular automaton" << endl;
+  cout << "2. Load a cellular automaton from a file" << endl;
+  cout << "0. Exit" << endl;
+  cout << "Choose an option from the list: ";
+}
+
+bool processMenu(int choice) {
+  switch(choice) {
+    case 1:
+      createAutomaton();
+      break;
+    case 2:
+      loadAutomaton();
+      break;
+    default:
+      return false;
+
+  }
+  return true;
+}
+
+void createAutomaton() {
+  // Creates the objects of the global object pointers
   Table* fullTable = new Table();
   Rule* ruleObj = new Rule();
   Generation* generationObj = new Generation();
 
   clearScreen();
+  // Gets the rule number from the user
   int rule =
       promptIntRange("Please enter the desired rule you want to use", 0, 255);
   int generations =
@@ -49,3 +102,8 @@ void runProgram() {
   // printArray(fullTable->getPTable(), fullTable->getArraySize());
   fullTable->printTable();
 }
+
+void loadAutomaton() {
+
+}
+
