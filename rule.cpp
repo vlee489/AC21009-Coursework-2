a3604b820c0bf118256a8db29344f30602db7bc1
Ramsay Sewell <sewellrf@gmail.com>
2019-11-07 22:07:09 +0000

    commented yipee

--- rule.cpp
+++ rule.cpp
@@ -7,57 +7,101 @@
 
 using namespace std;
 
+/**
+ * Converts an array of booleans, into Decimal
+ * @param binary[] the binary representation
+ * @param bits the number of bits 
+ * @return returns the decimal conversion
+ */
 int Rule::toDecimal(bool binary[], int bits) {
+  // checks if the array is NULL
   if (binary == NULL) {
     return UNEXPECTED_NULL_POINTER;
   }
+  // initialises decimal to 0
   int decimal = 0;
+
+  // calculates the decimal value from binary
   for (int i = bits - 1; i >= 0; i--) {
     if (binary[i] == true) {
       decimal += pow(2, bits - 1 - i);
     }
   }
+
+  // returns decimal
   return decimal;
 }
 
+/**
+ * Sets the Ruleset for the Cellular Automaton
+ * @param ruleNum takes in an inputted rule to set
+ * @return returns the validity of the rule
+ */
 int Rule::setRule(int ruleNum) {
+  // validates the ruleNum (8-bit)
   if (ruleNum < 0 || ruleNum > 255) {
     return RULE_NUM_OUT_RANGE;
   }
+  // initialises the instance's chosen rule
   this->ruleNum = ruleNum;
 
+  // converts rule num into it's binary representation
   int valid = toBinary(ruleSet, ruleNum);
 
   return valid;
 }
 
+/**
+ * Generates the value of a Cell in the Table
+ * @param neighbourhood[] the values of the current Cell's neighbourhood
+ * @return returns true of false for the generated Cell
+ */
 bool Rule::generateCell(bool neighbourhood[]) {
+  // converts the neighbourhood to decimal for the ruleset
   int index = toDecimal(neighbourhood, 3);
+
+  // makes sure that the neighbourhood was valid
   if (index == UNEXPECTED_NULL_POINTER) {
     return false;
   }
+
+  // returns the value of the ruleset at the specified index
   return ruleSet[7-index];
 }
 
+/**
+ * Converts a decimal into it's binary representation
+ * @param binary[] a pointer to the Ruleset
+ * @param decimal the number to be converted
+ * @return returns the validity of the conversion
+ */
 int Rule::toBinary(bool binary[], int decimal) {
+  // checks if ruleset is NULL
   if (binary == NULL) {
     return UNEXPECTED_NULL_POINTER;
   }
+
+  // makes sure that the decimal is in range
   if (decimal < 0 || decimal > 255) {
     return DECIMAL_VAL_OUT_OF_RANGE;
   }
-
+  // calculates the binary representation of the decimal
   for (int i = 0; decimal > 0; i++) {
     binary[i] = decimal % 2;
     decimal = decimal/2;
   }
 
+  // Reverses the order
   swap(binary, 8);
 
   return SUCCESS;
 }
 
-// Utility function to reverse elements of an array
+/**
+ * Reverses order of the binary representation
+ * @param arr[] the binary representation
+ * @param n the number of bits 
+ */
 void Rule::swap(bool arr[], int n) {
   reverse(arr, arr + n);
 }
--- rule.hpp
+++ rule.hpp
@@ -1,16 +1,24 @@
 #ifndef RULE_H
 #define RULE_H
 
+// Rule class will be used to store and calculate the Rule used in the Cellular Automaton
 class Rule {
  private:
+  // the chosen Rule
   int ruleNum;
+  // the binary representation of the Rule
   bool ruleSet[8];
 
  public:
+  // Utility function to convert decimal to Binary
   int toBinary(bool binary[], int decimal);
+  // Utility function to convert Binary to decimal
   int toDecimal(bool binary[], int bits);
+  // Sets the rule of the Automaton using the Rule
   int setRule(int ruleNum);
+  // Calculates the value of the next Cell
   bool generateCell(bool neighbourhood[]);
+  // Utility function to reverse the order of the binary representation
   void swap(bool arr[], int n);
 };
 
